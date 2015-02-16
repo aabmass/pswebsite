ded52cd4aeab32a94f5cb8ad5530615be9c6301e
Aaron Abbott <aabmass@gmail.com>
2015-02-16 02:16:30 -0500

    Setup menus in here

--- psbackend/__init__.py
+++ psbackend/__init__.py
@@ -2,4 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from psbackend import views
+from . import views
+from . import context
+from .menus import Menu
+
+# Create the menus
+menus = context.projectVars["menus"]
+menus.append(Menu("Home", "index"))
+
