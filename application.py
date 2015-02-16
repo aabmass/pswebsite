e357e13317af05617bf9c2825f8a32d7259de5b6
Aaron Abbott <aabmass@gmail.com>
2015-02-16 00:04:56 -0500

    Refactoring with a project

--- application.py
+++ application.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python3
 from flask import Flask
-from flask import render_template
+import views
 
 app = Flask(__name__)
-
-@app.route('/')
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
-
-@app.route('/generic')
-def generic():
-    return render_template('generic.html')
 
 if __name__ == '__main__':
     app.debug = True
