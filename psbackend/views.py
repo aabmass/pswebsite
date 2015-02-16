868d8dd288af799e83836bd1c17b8454e3779836
Aaron Abbott <aabmass@gmail.com>
2015-02-16 03:20:44 -0500

    Beginning support for logins
    Also, added pagetitle abilities

--- psbackend/views.py
+++ psbackend/views.py
@@ -1,5 +1,5 @@
 ########## Module with all of our views ##########
-from flask import url_for
+from flask import request
 
 from psbackend import app
 from . import templateutil
@@ -7,8 +7,14 @@ from . import templateutil
 @app.route('/')
 @app.route('/index')
 def index():
-    return templateutil.render('index.html')
+    return templateutil.render('index.html', pageTitle="Home")
 
-@app.route('/generic')
-def generic():
-    return templateutil.render('generic.html')
+@app.route('/about')
+def about():
+    return templateutil.render('about.html', pageTitle="About")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print(request.form)
+    return templateutil.render('login.html', pageTitle="Login")
+            #extraJsFiles=["js/login.js"])
