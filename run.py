3c1e88a3904bb403459ad14b9f3734fb59d9b01d
Aaron Abbott <aabmass@gmail.com>
2015-02-24 20:52:19 -0500

    Added a wrapper script (bash) to start execution
    Now, start the app with ./run.sh
    
    Run.sh sources some environment variables and can do other set up if
    necessary.

--- run.py
+++ run.py
@@ -2,8 +2,10 @@
 import os
 from psbackend import app
 
+# Start the app
 if ('PRODUCTION' in os.environ and
         os.environ['PRODUCTION'] == 'TRUE'):
     app.run(debug=False, host='0.0.0.0')
 else:
     app.run(debug=True)
+
