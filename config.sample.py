7be95e0029a9043ba6da28c9b1cd51a3d16b85e4
Aaron Abbott <aabmass@gmail.com>
2015-02-25 21:34:46 -0500

    Added a sample config file

---
+++ config.sample.py
@@ -0,0 +1,11 @@
+# Sample config file for the application
+
+# Copy this file to config.py in the project's root directory
+# and fill in the blanks
+
+dbuser = ""
+dbname = ""
+dbpass = ""
+
+# environemnt can either "production" or "debug"
+environment = ""
