c190af4f102d38bb1dbc79ff77184ded28cf5d98
Aaron Abbott <aabmass@gmail.com>
2015-02-25 21:17:48 -0500

    Generated a real secret key

--- psbackend/__init__.py
+++ psbackend/__init__.py
@@ -6,7 +6,7 @@ import os
 
 ## Flask init
 app = Flask(__name__)
-app.secret_key = "ROOISMYROOANDDUCKISDUCK"
+app.secret_key = 'z\xda\xed2\x1aI:\x14\xe6\xc1\xa3\xea\xd6n\x01\xad\xda\xdf\xe8\xd7\x83|\xc6\xe2'
 
 ## DB Init
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://psflask:{}@localhost/psdev'.format(os.environ["DBPASS"])
