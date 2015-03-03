d8c0bace23879d991af190be802782a9c09a0cdc
Aaron Abbott <aabmass@gmail.com>
2015-03-03 00:03:09 -0500

    Made solution mobile friendly. Caveats below:
    Unfortunately this solution doesn't work when resizing from larger than
    mobile threshold to small than it (an esoteric case). It does work from
    smaller to larger and when the page is loaded and not resized fine,
    however.
    
    This needs a fix. It will probably mean switching from dropotron to
    something else since dropotron doesn't allow for deactivating or
    unloading itself after initialization.

--- static/js/init.js
+++ static/js/init.js
@@ -79,14 +79,25 @@
             // Disable animations/transitions until the page has loaded.
             $body.addClass('is-loading');
 
-            $('#nav > ul').dropotron({
-                offsetX: 100 //move submenus over by offsetX
-            });
-
 			$window.on('load', function() {
 				$body.removeClass('is-loading');
 			});
 
+            var startDropotron = function() {
+                $("#nav > ul").dropotron({
+                    baseZIndex:         10005,
+                    alignment:          'right'
+                });
+            }
+            
+            // Start dropotron when medium
+            skel.on('!medium', startDropotron);
+            skel.on('-medium', startDropotron);
+
+            // When resized to mobile
+            skel.on('+medium', function() {
+                //TODO: Code to stop dropotron here
+            });
 	});
 
 })(jQuery);
